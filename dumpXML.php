0d028349db1a6946f6c54390f374ee67943eef42
r.r.kras <r.r.kras@sunyocc.edu>
2021-09-21 07:46:01 -0400

    reworked dumpXML.php, fixes to sqlite_db support

--- dumpXML.php
+++ dumpXML.php
@@ -1,21 +1,28 @@
 <?php
-include("inc/sessions.php");
-include("inc/db.php");
+include("inc/config.php");
 include("inc/crypt.php");
 define ('C_VERSION','1.0');
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
-header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+
+function xml_start(){
+
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+    header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+    header('Content-Type: application/xml');
+    $output = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+    $output .= "<!-- PHPchain password extract -->\n";
+
+    return $output;
+}
 
 sql_conn();
-
 
 $auth= is_authed();
 
 if (!$auth) {
-        header("Location: index.php");
+        echo "Not authenticated";
         die();
 }
 
@@ -23,31 +30,31 @@ $userid=$_SESSION["id"];
 $key=$_SESSION["key"];
 $login=$_SESSION["login"];
 
+if ($login != 'test'){
+    echo "No soup for you";
+    die();
+}
 
 function getSectionTitles($db, $userid)
 {
-        $result=mysqli_query($db, "select id, title from cat where userid = \"$userid\"");
-        while ($row=mysqli_fetch_row($result)) {
-        $titles [ $row[0]] = htmlspecialchars($row[1]);
+        $result=sql_query($db, "select id, title from cat where userid = \"$userid\"");
+        while ($row=sql_fetch_row($result)) {
+            $titles [ $row[0]] = htmlspecialchars($row[1]);
         }
         return $titles;
 }
 
-
-$acctName = $login;
 $titles = getSectionTitles($db, $userid);
 
-header('Content-Type: application/xml');
-$output = '<?xml version="1.0" encoding="UTF-8"?>';
-$output .= "<!-- PHPchain password extract -->";
+$output = xml_start();
 
-$result=mysqli_query($db, "select id, catid, iv, login, password, site, url from logins where userid = \"$userid\"");
+$result=sql_query($db, "select id, catid, iv, login, password, site, url from logins where userid = \"$userid\"");
 
-if (mysqli_num_rows($result)==0) {
-    $output.="<error>No data found</error>";
+if (sql_num_rows($result)==0) {
+    $output.="<error>No data found</error>\n";
 } else {
-    $output .= '<pwlist>';
-    while ($row=mysqli_fetch_assoc($result)) {
+    $output .= "<pwlist>\n";
+    while ($row=sql_fetch_assoc($result)) {
 
         $login=trim(decrypt($key,base64_decode($row["login"]),base64_decode($row["iv"])));
         $password=htmlspecialchars(trim(decrypt($key,base64_decode($row["password"]),base64_decode($row["iv"]))));
@@ -55,17 +62,17 @@ if (mysqli_num_rows($result)==0) {
         $url=trim(decrypt($key,base64_decode($row["url"]),base64_decode($row["iv"])));
 	    $cat = trim($row["catid"]);
 
-        $output .= "<pwentry>";
-        $output .= "<group>" .  $titles[$cat] . "</group>";
-        $output .= '<notes>Group ' .  $row["id"] . " from " . $titles[$cat].  '</notes>';
-        $output .= '<title>' .  $site .  '</title>';
-        $output .= '<username>' . $login . '</username>';
-        $output .= '<password>'  . $password .  '</password>';
-        $output .= '<url>' .  $url . '</url>';
-        $output .="</pwentry>";
+        $output .= "   <pwentry>\n";
+        $output .= "      <group>" .  $titles[$cat] . "</group>\n";
+        $output .= "      <notes>Group " .  $row["id"] . " from " . $titles[$cat].  "</notes>\n";
+        $output .= "      <title>" .  $site .  "</title>\n";
+        $output .= "      <username>" . $login . "</username>\n";
+        $output .= "      <password>"  . $password .  "</password>\n";
+        $output .= "      <url>" .  $url . "</url>\n";
+        $output .= "   </pwentry>\n";
 
     }
-    $output .= '</pwlist>';
+    $output .= "</pwlist>\n";
 }
 
 echo $output;
--- inc/sqlite_db.php
+++ inc/sqlite_db.php
@@ -70,55 +70,78 @@ function sql_insert_id($db)
 	return $db->lastInsertRowId();
 }
 
-// function sql_prepare($db, $query)
-// {
-// 	return $db->prepare($query);
-// }
+function sql_prepare($db, $query)
+{
+	return $db->prepare($query);
+}
 
-// function sql_stmt_bind_value($stmt, $param, $value, $type)
-// {
-// 	return $stmt->bindValue($param, $value, $type);
-// }
+function sql_stmt_bind_value($stmt, $param, $value, $type)
+{
+	return $stmt->bindValue($param, $value, $type);
+}
 
-// function sql_stmt_bind_param($stmt, $param, &$var, $type)
-// {
-// 	return $stmt->bindParam($param, $var, $type);
-// }
+function sql_stmt_bind_param($stmt, $param, &$var, $type=0)
+{
+	if ($type) return $stmt->bindParam($param, $var, $type);
+	else return $stmt->bindParam($param,$var);
+}
 
-// function sql_stmt_multi_bind_param($stmt, $types, &...$vars)
-// {
-// 	$type_lists = array(
-// 		'i' => 'SQLITE3_INTEGER',
-// 		'd' => 'SQLITE3_FLOAT',
-// 		's' => 'SQLITE3_TEXT',
-// 		'b' => 'SQLITE3_BLOB',
-// 	);
+function sql_stmt_multi_bind_param($stmt, &...$vars)
+{
+	$param = 1;
+	$res = True;
+	foreach($vars as &$var){
+		$res = $res AND $stmt->bindParam($param, $var);
+		$param +=1;
+	}
+}
 
-// 	$param = 0;
-// 	$res = TRUE;
-// 	foreach ($vars as &$var){
-// 		$type = $types[$param];
-// 		$param += 1;
-// 		$res = $res AND $stmt->bindParm($param, $var, $type);
-// 		if (!$res) break;
-// 	}
-// 	return $res;
-// }
+function sql_stmt_multi_bind_value($stmt, ...$vars)
+{
+	$param = 1;
+	$res = True;
+	foreach($vars as $var){
+		$res = $res AND $stmt->bindValue($param, $var);
+		$param +=1;
+	}
+}
 
-// function sql_stmt_execute($stmt)
-// {
-// 	// error_log('sqlite stmt exec: ' . $stmt->getSql(TRUE));
-// 	return $db->execute();
-// }
+function sql_mysql_multi_bind_param($stmt, $types, &...$vars)
+{
+	$type_lists = array(
+		'i' => SQLITE3_INTEGER ,
+		'd' => SQLITE3_FLOAT,
+		's' => SQLITE3_TEXT,
+		'b' => SQLITE3_BLOB,
+	);
 
-// function sql_stmt_clear($stmt)
-// {
-// 	return $stmt->clear();
-// }
+	$param = 0;
+	$res = TRUE;
+	foreach ($vars as &$var){
+		$type = $types[$param];
+		$param += 1;
+		$res = $res AND $stmt->bindParam($param, $var, $type_lists[$type]);
+		if (!$res) break;
+	}
+	return $res;
+}
 
-// function sql_stmt_close($stmt)
-// {
-// 	return $stmt->close();
-// }
+function sql_stmt_execute($stmt)
+{
+	return $stmt->execute();
+}
+
+function sql_stmt_reset($stmt){
+	return $stmt->reset();
+}
+function sql_stmt_clear($stmt)
+{
+	return $stmt->clear();
+}
+
+function sql_stmt_close($stmt)
+{
+	return $stmt->close();
+}
 
 ?>
